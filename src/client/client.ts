4f3d8483249a1a84255660214ed2f9d232a4b4da
Bogdan Salau <bogdan.salau@cutr.ai>
2022-12-13 20:37:09 +0200

    da

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,8 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Key } from 'ts-keycode-enum'
 
 const scene = new THREE.Scene()
-
+scene.background = new THREE.Color( 0xff0000 );
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2
 
@@ -12,14 +13,35 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+const geometry = new THREE.CircleGeometry(20);
+const material = new THREE.MeshLambertMaterial({
+    color: 0xffffff,
+    wireframe: false,
+});
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const disc = new THREE.Mesh(geometry, material)
+scene.add(disc)
+
+
+function setupKeyControls() {
+    document.onkeydown = function(e) {
+        let geom;
+        switch (e.keyCode) {
+            case Key.DownArrow:
+                break;
+            case Key.UpArrow:
+                break;
+            case Key.RightArrow:
+                disc.rotation.x -= 0.1;
+                break;
+            case Key.LeftArrow:
+                disc.rotation.z += 0.1;
+                break;
+        }
+    };
+}
+
+setupKeyControls();
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
