25fd33b5d86cce5fc9dcbd938d61a204cf71d218
Bogdan Salau <bogdan.salau@cutr.ai>
2022-12-13 20:43:34 +0200

    textured disc

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,11 +1,12 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Key } from 'ts-keycode-enum'
+import img from './img'
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0xff0000 );
+scene.background = new THREE.Color( 0xffffff );
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+camera.position.z = 100
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -13,11 +14,15 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const geometry = new THREE.CircleGeometry(20);
-const material = new THREE.MeshLambertMaterial({
-    color: 0xffffff,
-    wireframe: false,
-});
+
+const texture = new THREE.TextureLoader().load(img)
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set( 4, 4 );
+
+
+const geometry = new THREE.CylinderGeometry(20, 20, 2, 100);
+const material = new THREE.MeshBasicMaterial({ map: texture });
 
 const disc = new THREE.Mesh(geometry, material)
 scene.add(disc)
@@ -54,8 +59,8 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
 
     controls.update()
 
